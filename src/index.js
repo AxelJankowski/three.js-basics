caebd8ef290345be4f61651ed1c8540478e95da4
AxelJankowski <mpc@CL4P-TP.localdomain>
2020-09-18 15:12:31 +0200

    Dancing atom.

--- src/index.js
+++ src/index.js
@@ -2,44 +2,83 @@ import _ from 'lodash';
 import './style.css';
 import * as THREE from 'three';
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 50);
 
-var renderer = new THREE.WebGLRenderer();
+let renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const colorYellow = new THREE.Color('hsl(40, 100%, 60%)');
-const colorPink = new THREE.Color('hsl(306, 100%, 60%)');
+function handleResize() {
+    let { innerWidth, innerHeight } = window;
+    renderer.setSize(innerWidth, innerHeight);
+    camera.aspect = innerWidth / innerHeight;
+    camera.updateProjectionMatrix();
+}
 
-var cubeGeometry = new THREE.BoxGeometry(1, 1.2, 1);
-var cubeMaterial = new THREE.MeshPhongMaterial({
-	color: colorYellow,
-	shininess: 80,
-});
-var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-var light = new THREE.PointLight(colorPink, 2);
-var light2 = new THREE.PointLight(colorYellow, 2);
 
-light.position.z = 20;
-light.position.y = -20;
-light.position.x = -40;
 
-light2.position.z = 10;
-light2.position.y = 20;
-light2.position.x = 40;
+// Sphere creator.
+function createSphere(r = 1, color = 0xffffff) {
+    const sphereGeo = new THREE.SphereGeometry(r, 20, 20);
+    const sphereMat = new THREE.MeshPhongMaterial({
+        color,
+        shininess: 20,
+    });
+    return new THREE.Mesh(sphereGeo, sphereMat);
+}
+// Light creator.
+function createLight(i = 1, color = 0xffffff) {
+    return new THREE.PointLight(color, i);
+}
 
-scene.add(light);
-scene.add(light2);
-scene.add(cube);
+const nucleus = createSphere(4);
+const l1 = createLight(.2);
+const l2 = createLight();
+l1.position.set(120, 5, 10);
+l2.position.set(-60, 0, 20);
+scene.add(nucleus, l2);
+nucleus.add(l1);
 
-camera.position.z = 5;
+// Electron creator.
+function createElectron(r = 1, color = 0xffffff) {
+    const sphere = createSphere(r, color);
+    const pivot = new THREE.Object3D();
+    pivot.add(sphere);
+    return {
+        sphere,
+        pivot
+    }
+}
 
-cube.rotation.x = 20;
-cube.rotation.z = -20;
+const e1 = createElectron(0.5);
+const e2 = createElectron(0.5);
+const e3 = createElectron(0.5);
+const e4 = createElectron(0.5);
+e1.sphere.position.set(13, 0, 0);
+e2.sphere.position.set(8, 0, 0);
+e3.sphere.position.set(-8, 0, 0);
+e4.sphere.position.set(-13, 0, 0);
+nucleus.add(e1.pivot, e2.pivot, e3.pivot, e4.pivot);
+
+e1.pivot.rotation.y = 120;
+e2.pivot.rotation.y = 30;
+e3.pivot.rotation.y = 30;
+e4.pivot.rotation.y = 120;
+
 
 function animate() {
-	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
+    requestAnimationFrame(animate);
+    e1.pivot.rotation.z += 0.04;
+    e2.pivot.rotation.z += 0.03;
+    e3.pivot.rotation.z += 0.04;
+    e4.pivot.rotation.z += 0.03;
+    nucleus.rotation.z += 0.01;
+    nucleus.rotation.x += 0.02;
+    nucleus.rotation.y += 0.03;
+	renderer.render(scene, camera);
 }
+
 animate();
+window.addEventListener('resize', handleResize);
